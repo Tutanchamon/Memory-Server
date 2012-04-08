2e9aabbaef1cc877707696462391504f90161532
Błażej Kubiński <tutenchamon@gmail.com>
2012-04-09 00:54:50 +0200

    Dodalem wysylanie pliku

--- multi.c
+++ multi.c
@@ -20,6 +20,7 @@
 
 #define TRUE   1
 #define FALSE  0
+#define MAX_BUF 10240
 
 /* network functions */
 #include <sys/types.h>
@@ -32,6 +33,76 @@
 #include <sys/sendfile.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+
+/* metoda z cwiczen od Gogolewskiego */
+void ObsluzPolaczenie(int gn)
+{
+    char sciezka[512];
+    long dl_pliku, wyslano, wyslano_razem, przeczytano;
+    struct stat fileinfo;
+    FILE* plik;
+    unsigned char bufor[1024];
+
+   /* memset(sciezka, 0, 512);
+    if (recv(gn, sciezka, 512, 0) <= 0)
+    {
+        printf("Potomny: blad przy odczycie sciezki\n");
+        return;
+    }*/
+    strcpy(sciezka, "a.gif");
+
+    printf("Potomny: klient chce plik %s\n", sciezka);
+
+    if (stat(sciezka, &fileinfo) < 0)
+    {
+        printf("Potomny: nie moge pobrac informacji o pliku\n");
+        return;
+    }
+
+    if (fileinfo.st_size == 0)
+    {
+        printf("Potomny: rozmiar pliku 0\n");
+        return;
+    }
+
+    printf("Potomny: dlugosc pliku: %d\n", fileinfo.st_size);
+
+    dl_pliku = htonl((long) fileinfo.st_size);
+
+   /* if (send(gn, &dl_pliku, sizeof(long), 0) != sizeof(long))
+    {
+        printf("Potomny: blad przy wysylaniu wielkosci pliku\n");
+        return;
+    }*/
+
+    dl_pliku = fileinfo.st_size;
+    wyslano_razem = 0;
+    plik = fopen(sciezka, "rb");
+    if (plik == NULL)
+    {
+        printf("Potomny: blad przy otwarciu pliku\n");
+        return;
+    }
+
+    while (wyslano_razem < dl_pliku)
+    {
+        przeczytano = fread(bufor, 1, 1024, plik);
+        wyslano = send(gn, bufor, przeczytano, 0);
+        if (przeczytano != wyslano)
+            break;
+        wyslano_razem += wyslano;
+        printf("Potomny: wyslano %d bajtow\n", wyslano_razem);
+    }
+
+    if (wyslano_razem == dl_pliku)
+        printf("Potomny: plik wyslany poprawnie\n");
+    else
+        printf("Potomny: blad przy wysylaniu pliku\n");
+    fclose(plik);
+    return;
+}
+/*********************************/
+
 
 int main()
 {
@@ -46,13 +117,20 @@ int main()
   int max_clients=3;
 
   int activity, loop, loop2, valread;
-  char buffer[1025];    /* data buffer of 1K */
+  char buffer[10240];    /* data buffer of 1K */
 
-  char filename[1025];
-    int fd; // numer deskryptora plików
+  char filename[10240];
     struct stat stat_buf;      /* argument to fstat */
     int rc;
     int offset = 0;
+    int i;
+
+    // te z polibudy
+    int fd;
+	int count_r, count_w;
+	char* bufptr;
+	char buf[MAX_BUF];
+
 
   fd_set readfds;
 
@@ -131,12 +209,16 @@ int main()
       printf("New socket is fd %d\n",new_socket);
 
 /* transmit message to new connection */
-      if (send(new_socket, message, strlen(message), 0) != strlen(message)) {
+/*      if (send(new_socket, message, strlen(message), 0) != strlen(message)) {
 /* if send failed to send all the message, display error and exit */
-        perror("send");
-      }
-      puts("Welcome message sent successfully");
+     /*   perror("send");
+      }*/
+     // else printf("\nWiadomosc wyslana");
+
+     // puts("Welcome message sent successfully");
+
 /* add new socket to list of sockets */
+        ObsluzPolaczenie(new_socket);
       for (loop=0; loop<max_clients; loop++) {
         if (client_socket[loop] == 0) {
           client_socket[loop] = new_socket;
@@ -149,7 +231,7 @@ int main()
         printf("Idzie petla z loop\n");
       if (FD_ISSET(client_socket[loop], &readfds)) {
           // dodane ode mnie
-        if ((valread = recv(client_socket[loop], buffer, 1024, 0)) < 0) { // bylo read zamiast recv
+        if ((valread = recv(client_socket[loop], buffer, 10240, 0)) < 0) { // bylo read zamiast recv
           printf("Wywolal sie fragment, ktorego nie czaje\n");
           close(client_socket[loop]);
           client_socket[loop] = 0;
@@ -157,9 +239,12 @@ int main()
 /* set the terminating NULL byte on the end of the data read */
 
           buffer[valread] = '\0';
-          // dodane do obslugi plikow
 
-          strcpy(filename, buffer);
+
+          // dodane do obslugi plikow
+            // ale jednak i tak coś nie dziala
+            // wiec do zakomentowania
+          /*strcpy(filename, buffer);
           printf("\nFilename to: %s\n", filename);
           filename[valread] = '\0';
           if (filename[strlen(filename)-1] == '\n')
@@ -168,14 +253,16 @@ int main()
             filename[strlen(filename)-1] = '\0';
 
             /* open the file to be sent */
-            fd = open(filename, O_RDONLY);
+
+            /*fd = open(filename, O_RDONLY);
             if (fd == -1) {
               fprintf(stderr, "unable to open '%s': %s\n", filename, strerror(errno));
               exit(1);
             }
 
             /* get the size of the file to be sent */
-            fstat(fd, &stat_buf);
+           // fstat(fd, &stat_buf);
+
 
 
 
@@ -183,7 +270,7 @@ int main()
 
 
           // to bylo dodane do osblugi plikow
-
+            printf("\nWchodze do loop2");
           for (loop2=0; loop2<max_clients; loop2++) {
 /* note, flags for send() are normally 0, see man page for details */
             printf("Tera idzie po loop2\n");
@@ -191,7 +278,7 @@ int main()
           /**********************************************/
           /* wrzucone zamiast senda by wyslac plik      */
 
-          /* copy file using sendfile */
+          /* copy file using sendfile
             offset = 0;
             rc = sendfile (client_socket[loop2], fd, (off_t*) &offset, stat_buf.st_size);
             if (rc == -1) {
@@ -208,6 +295,8 @@ int main()
 
 
           /*********************************************/
+
+
 
           }
           printf("Idzie memset\n");
@@ -239,3 +328,4 @@ int main()
 /* normally you should close the sockets here before program exits */
 /* however, in this example the while() loop prevents getting here */
 }
+
